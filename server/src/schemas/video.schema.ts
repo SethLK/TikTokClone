c1bc6e186f662ddf9e53396df4e0fefa01f00dd2
SethLK <sethlk2006@gmail.com>
2024-08-04 17:29:17 +0630

    Backend for Auth is done and Video is ready

---
+++ server/src/schemas/video.schema.ts
@@ -0,0 +1,37 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { Document, Types } from "mongoose";
+
+export type VideoDocument = mongoose.HydratedDocument<Video>;
+
+@Schema()
+export class Video {
+    @Prop({ type: String, required: true })
+    video_url: string;
+
+    @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+    author: Types.ObjectId;
+
+    @Prop({ type: Number, default: 0 })
+    likes_count: number;
+
+    @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }] })
+    likeByUsers: Types.ObjectId[];
+
+    @Prop({ type: Date, default: Date.now })
+    date: Date;
+
+    @Prop({ type: String, default: "" })
+    description: string;
+
+}
+
+export const VideoSchema = SchemaFactory.createForClass(Video);
+
+export type VideoType = {
+    _id: string,
+    video_url: string,
+    description: string,
+    likes_count: number,
+    likeByUsers: Types.ObjectId[];
+    author: Types.ObjectId;
+};
