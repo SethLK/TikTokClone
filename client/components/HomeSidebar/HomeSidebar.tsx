b21cf92c6bee82d3d8bce2dd58fcf45421a90024
SethLK <sethlk2006@gmail.com>
2024-08-13 20:25:58 +0630

    home to login link added

--- client/components/HomeSidebar/HomeSidebar.tsx
+++ client/components/HomeSidebar/HomeSidebar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import HomeSideFooter from "../HomeSideFooter/HomeSideFooter";
 import HomeSideNav from "../HomeSideNav/HomeSideNav";
 import "./scrollbar.css";
@@ -9,9 +10,9 @@ export default function HomeSidebar() {
       <p className="text-neutral-400 flex items-center w-full h-[7vh] px-4 font-semibold text-left my-10">
         Log in to follow creators, like videos, and view comments.
       </p>
-      <div className="text-red-500 font-semibold text-lg flex items-center justify-center w-auto h-[7vh] mx-4 border border-red-500 rounded-md bg-neutral-800 hover:bg-neutral-900 cursor-pointer">
+      <Link href={"/login"} className="text-red-500 font-semibold text-lg flex items-center justify-center w-auto h-[7vh] mx-4 border border-red-500 rounded-md bg-neutral-800 hover:bg-neutral-900 cursor-pointer">
         Log in
-      </div>
+      </Link>
       <HomeSideFooter />
     </div>
   );
